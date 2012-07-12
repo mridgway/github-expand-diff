e52ae130641ccd92a644be3d287dbd8519f83edc
Michael Ridgway <mcridgway@gmail.com>
2012-07-11 19:53:16 -0700

    Big refactor. Should make it more stable and more optimized, although iit is still not super fast.

--- github-expand-diff.user.js
+++ github-expand-diff.user.js
@@ -7,24 +7,35 @@
 
 (function () {
     var codeGaps = findCodeGaps(),
-        fileDoms = {};
+        fileDoms = {},
+        parseNode = createParseNode(),
+        LOADING_HTML = '<span style="font-size: 1.6em">&#8987;</span> Loading...';
+
     for (var i=0; i<codeGaps.length; ++i) {
         bindClick(codeGaps[i]);
     }
 
+    function createParseNode () {
+        var parseNode = document.createElement('span');
+        parseNode.style.display = 'none';
+        parseNode.id = 'gde-parseNode';
+        return parseNode;
+    }
+
     function findCodeGaps () {
         var codeGaps = [],
-            files = document.getElementById('files').getElementsByClassName('file');
+            files = getFileNodes();
 
         for (var i=0; i<files.length; ++i) {
             var fileLine = 0,
-                lines = files[i].getElementsByTagName('tr');
+                lines = getLineNodes(files[i]),
+                fileUrl = getFileUrl(files[i]),
+                lastLineNode = createLastLineNode(files[i]);
+
             for (var j=0; j<lines.length; ++j) {
-                var lineNumber = lines[j].getElementsByClassName('line_numbers')[1].firstChild.nodeValue.replace(/^\s+|\s+$/g, '');
+                var lineNumber = getLineNumber(lines[j]);
                 if ('...' === lineNumber) {
-                    var fileLinks = files[i].getElementsByClassName('meta')[0].getElementsByClassName('minibutton')[0],
-                        fileUrl = files[i].getElementsByClassName('meta')[0].getElementsByClassName('minibutton')[0].href,
-                        nextLineNumber = lines[j+1].getElementsByClassName('line_numbers')[1].firstChild.nodeValue.replace(/^\s+|\s+$/g, '');
+                    var nextLineNumber = getLineNumber(lines[j+1]);
 
                     codeGaps.push({
                         lineNode: lines[j],
@@ -36,8 +47,65 @@
                     fileLine = parseInt(lineNumber, 10);
                 }
             }
+            files[i].getElementsByTagName('tbody')[0].appendChild(lastLineNode);
+            codeGaps.push({
+                lineNode: lastLineNode,
+                startLine: fileLine + 1,
+                fileUrl: fileUrl
+            });
         }
         return codeGaps;
+    }
+
+    function getFileNodes () {
+        var fileNodes = [],
+            filesNode = document.getElementById('files');
+        if (filesNode) {
+            fileNodes = filesNode.getElementsByClassName('file');
+        }
+        return fileNodes;
+    }
+
+    function getLineNodes (fileNode) {
+        var lines = fileNode.getElementsByTagName('tr');
+        return lines;
+    }
+
+    function getFileUrl (fileNode) {
+        var metaNodes = fileNode.getElementsByClassName('meta'),
+            miniButtonNodes;
+
+        if (!metaNodes) {
+            return null;
+        }
+
+        miniButtonNodes = metaNodes[0].getElementsByClassName('minibutton');
+
+        if (!miniButtonNodes) {
+            return null;
+        }
+
+        return miniButtonNodes[0].href;
+    }
+
+    function createLastLineNode () {
+        var lastLineNode = document.createElement('tr');
+        lastLineNode.innerHTML = '<td class="line_numbers">...</td><td class="line_numbers">...</td><td class="gc diff-line"><pre class="line"></pre></td>';
+        return lastLineNode;
+    }
+
+    function getLineNumber (lineNode) {
+        var lineNumbers;
+
+        if (!lineNode) {
+            return null;
+        }
+
+        lineNumbers = lineNode.getElementsByClassName('line_numbers');
+        if (lineNumbers && lineNumbers.length > 1) {
+            return lineNumbers[1].firstChild.nodeValue.replace(/^\s+|\s+$/g, '');
+        }
+        return null;
     }
 
     function bindClick (codeGap) {
@@ -52,7 +120,7 @@
                 codeGap.open = false;
             } else {
                 codeGap.oldLine = codeNode.innerHTML;
-                codeNode.innerHTML = '<span style="font-size: 1.6em">&#8987;</span> Loading...';
+                codeNode.innerHTML = LOADING_HTML;
                 getCode(codeGap, function (code) {
                     codeNode.innerHTML = codeGap.newLine = code;
                 });
@@ -79,14 +147,28 @@
     }
 
     function parseDom (codeGap, dom) {
-        var code = '';
+        var code = '',
+            domParseNode;
+
+        parseNode.innerHTML = dom;
+        domParseNode = document.body.appendChild(parseNode);
+        if (undefined === codeGap.endLine) {
+            var lastLineRegex = /([0-9]*) lines \(([0-9]*) sloc\)/i,
+                file = domParseNode.getElementsByClassName('file')[0],
+                metaNode = file.getElementsByClassName('meta')[0];
+            codeGap.endLine = metaNode.innerHTML.match(lastLineRegex)[1];
+        }
         for (var i=codeGap.startLine; i<=codeGap.endLine; ++i) {
-            var regex = new RegExp("<div class='line' id='LC"+i+"'>(.*)</div><div class='line' id='LC"+(i+1)+"'>", 'i'),
-                match = dom.match(regex),
-                line = match ? match[1] : '';
-            var lineHtml = '<tr><td class="line_numbers">&nbsp;</td><td class="line_numbers">'+i+'</td><td class="gi diff-line"><pre class="line"></pre></td></tr>';
+            var lineNode = document.getElementById('LC' + i),
+                line = lineNode ? lineNode.innerHTML : '';
             code += ' ' + line + '\n';
         }
+        document.body.removeChild(domParseNode);
+        parseNode.innerHTML = '';
         return code;
+    }
+
+    function logTime () {
+        console.log((new Date()).getTime());
     }
 })();

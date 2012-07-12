7d3358e4b86a16936805d954a22a9d65dda26b73
Michael Ridgway <mcridgway@gmail.com>
2012-07-11 20:00:46 -0700

    Adding README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Diff Expander
+
+github-diff-expander.user.js is a user/Greasemonkey script for Chrome and Firefox that allows you to expand the missing lines in a Github diff.
--- github-expand-diff.user.js
+++ github-expand-diff.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name Github-Diff-Expander
+// @name Github Diff Expander
 // @match https://github.com/*/commit/*
 // @match https://github.com/*/pull/*
 // @version 1.0

508c4a4646f58fa540ac21a21f63f3cc7c2d7d7a
Michael Ridgway <mridgway@yahoo-inc.com>
2013-10-15 10:51:31 -0700

    v1.5

--- github-expand-diff.user.js
+++ github-expand-diff.user.js
@@ -4,7 +4,7 @@
 // @match https://github.com/*/commit/*
 // @match https://github.com/*/pull/*
 // @match https://github.com/*/compare/*
-// @version 1.4
+// @version 1.5
 // ==/UserScript==
 
 (function () {
